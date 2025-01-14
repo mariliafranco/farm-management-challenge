b646b571b63182094dd74a6957568460c131a63b
mariliafranco <marilia.andrad@gmail.com>
2025-01-14 10:05:59 -0300

    handle errors related to the http requests

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -4,23 +4,34 @@ import { CropType, Farm } from "../types/Farm";
 const API_BASE_URL = "http://localhost:3001";
 
 export const getFarms = async (): Promise<Farm[]> => {
-  const response = await axios.get<Farm[]>(`${API_BASE_URL}/farms`);
-
-  return response.data;
+  try {
+    const response = await axios.get<Farm[]>(`${API_BASE_URL}/farms`);
+    return response.data;
+  } catch (error: unknown) {
+    console.error("Error fetching farms:", error);
+    throw error;
+  }
 };
 
 export const getCropTypes = async (): Promise<CropType[]> => {
-  const response = await axios.get<CropType[]>(`${API_BASE_URL}/crop-types`);
-
-  return response.data;
+  try {
+    const response = await axios.get<CropType[]>(`${API_BASE_URL}/crop-types`);
+    return response.data;
+  } catch (error: unknown) {
+    console.error("Error fetching crops:", error);
+    throw error;
+  }
 };
 
 export const addFarm = async (newFarm: Omit<Farm, "id">) => {
-  const response = await axios.post<Farm>(`${API_BASE_URL}/farms`, newFarm);
-
-  console.log("New farm created!", response.data);
-
-  return response.data;
+  try {
+    const response = await axios.post<Farm>(`${API_BASE_URL}/farms`, newFarm);
+    console.log("New farm created!", response.data);
+    return response.data;
+  } catch (error: unknown) {
+    console.error("Error creating farm:", error);
+    throw error;
+  }
 };
 
 export const deleteFarm = async (id: string): Promise<void> => {
